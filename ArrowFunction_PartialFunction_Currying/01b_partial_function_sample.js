c6b5085c15e79011b0f501523c131de1e43454d2
Daniel Calábria <43326008+danielcalabria@users.noreply.github.com>
2020-11-22 01:48:50 -0300

    Update 01b_partial_function_sample.js

--- ArrowFunction_PartialFunction_Currying/01b_partial_function_sample.js
+++ ArrowFunction_PartialFunction_Currying/01b_partial_function_sample.js
@@ -2,7 +2,7 @@ function addition(x, y){
 	return x + y;
 }
 
-const plus5 = addition.bind(x, 5)
-plus5(10)
+const plus5 = addition.bind(x, 5) // uma nova função fixando um argumento da função addition
+plus5(10) // chamando a função com apenas um argumento
 
 // output -> 15
